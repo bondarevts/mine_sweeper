e941c41829c10e3b3f231821a570edace11272ab
Timofey Bondarev <bondarevts@gmail.com>
2015-04-04 07:19:02 +0000

    Add tkinter support. Now with GUI!

--- main.py
+++ main.py
@@ -1,59 +1,30 @@
 import random
+import tkinter as tk
+from tkinter import messagebox
+import sys
 
 
 def main():
-    field = [[0] * 10 for _ in range(10)]
-    for i in range(4):
-        field[random.randrange(10)][random.randrange(10)] = 9
-
-    for i in range(10):
-        for j in range(10):
-            if field[i][j] == 9:
-                for ii in range(i - 1, i + 2):
-                    for jj in range(j - 1, j + 2):
-                        if 0 <= ii < 10 and 0 <= jj < 10 and field[ii][jj] != 9:
-                            field[ii][jj] += 1
-
-    print('-'*12)
-    for i in range(10):
-        print('|', end='')
-        for j in range(10):
-            if field[i][j] >= 0:
-                print(' ', end='')
-            elif field[i][j] == -1:
-                print('_', end='')
-            else:
-                print(-field[i][j] - 1, end='')
-        print('|')
-    print('-'*12)
-
-    closed = 96
-
-    while closed:
-        user_step = input('Input new step: ')
-        parts = user_step.split()
-        i = int(parts[0])
-        j = int(parts[1])
-
+    def button_click(event):
+        nonlocal closed
+        i = event.x // 50
+        j = event.y // 50
         if field[i][j] == 9:
-            print('Game over!')
-            print('-'*12)
-            for i in range(10):
-                print('|', end='')
-                for j in range(10):
-                    if field[i][j] < 0:
-                        field[i][j] = -field[i][j] + 1
-                    if field[i][j] == 9:
-                        print('*', end='')
-                    elif field[i][j] == 0:
-                        print(' ', end='')
+            for ii in range(10):
+                for jj in range(10):
+                    if field[ii][jj] < 0:
+                        c.create_image(ii * 50, jj * 50, anchor=tk.NW, image=imgs[-field[ii][jj] - 1])
                     else:
-                        print(field[i][j], end='')
-                print('|')
-            print('-'*12)
-            break
+                        c.create_image(ii * 50, jj * 50, anchor=tk.NW, image=imgs[field[ii][jj]])
+            for ii in range(10):
+                c.create_line(ii * 50, 0, ii * 50, 500)
+                c.create_line(0, ii * 50, 500, ii * 50)
+
+            messagebox.showinfo(message='Game over!')
+            sys.exit(0)
+
         if field[i][j] < 0:
-            continue
+            return
 
         if field[i][j] == 0:
             check_pos = [(i, j)]
@@ -67,38 +38,64 @@ def main():
                     if field[ci][cj] >= 0:
                         field[ci][cj] = -field[ci][cj] - 1
                         closed -= 1
-            print('-'*12)
-            for i in range(10):
-                print('|', end='')
-                for j in range(10):
-                    if field[i][j] >= 0:
-                        print(' ', end='')
-                    elif field[i][j] == -1:
-                        print('_', end='')
+            for ii in range(10):
+                for jj in range(10):
+                    if field[ii][jj] < 0:
+                        c.create_image(ii * 50, jj * 50, anchor=tk.NW, image=imgs[-field[ii][jj] - 1])
                     else:
-                        print(-field[i][j] - 1, end='')
-                print('|')
-            print('-'*12)
-            continue
+                        c.create_image(ii * 50, jj * 50, anchor=tk.NW, image=imgs[' '])
+            for ii in range(10):
+                c.create_line(ii * 50, 0, ii * 50, 500)
+                c.create_line(0, ii * 50, 500, ii * 50)
+
+            if closed == 0:
+                messagebox.showinfo(message='Winner!')
+                sys.exit()
+            return
 
         field[i][j] = -field[i][j] - 1
         closed -= 1
-
-        print('-'*12)
-        for i in range(10):
-            print('|', end='')
-            for j in range(10):
-                if field[i][j] >= 0:
-                    print(' ', end='')
-                elif field[i][j] == -1:
-                    print('_', end='')
+        for ii in range(10):
+            for jj in range(10):
+                if field[ii][jj] < 0:
+                    c.create_image(ii * 50, jj * 50, anchor=tk.NW, image=imgs[-field[ii][jj] - 1])
                 else:
-                    print(-field[i][j] - 1, end='')
-            print('|')
-        print('-'*12)
-    else:
-        print('Winner!')
+                    c.create_image(ii * 50, jj * 50, anchor=tk.NW, image=imgs[' '])
+        for ii in range(10):
+            c.create_line(ii * 50, 0, ii * 50, 500)
+            c.create_line(0, ii * 50, 500, ii * 50)
+        if closed == 0:
+            messagebox.showinfo(message='Winner!')
+            sys.exit()
 
+    root = tk.Tk()
+    c = tk.Canvas(root, width=500, height=500)
+    c.pack()
+    imgs = {i: tk.PhotoImage(file='imgs/{}.png'.format(i)) for i in range(10)}
+    imgs[9] = tk.PhotoImage(file='imgs/bomb.png')
+    imgs[' '] = tk.PhotoImage(file='imgs/empty.png')
+
+    field = [[0] * 10 for _ in range(10)]
+    for i in range(4):
+        field[random.randrange(10)][random.randrange(10)] = 9
+
+    for i in range(10):
+        for j in range(10):
+            if field[i][j] == 9:
+                for ii in range(i - 1, i + 2):
+                    for jj in range(j - 1, j + 2):
+                        if 0 <= ii < 10 and 0 <= jj < 10 and field[ii][jj] != 9:
+                            field[ii][jj] += 1
+
+    for i in range(10):
+        for j in range(10):
+            c.create_image(i * 50, j * 50, anchor=tk.NW, image=imgs[' '])
+    for i in range(10):
+        c.create_line(i * 50, 0, i * 50, 500)
+        c.create_line(0, i * 50, 500, i * 50)
+    closed = 96
+    c.bind('<Button-1>', button_click)
+    root.mainloop()
 
 if __name__ == '__main__':
     main()
