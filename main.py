de6526a174539c48eeeb1dad9a92c26917aff45b
Timofey Bondarev <bondarevts@gmail.com>
2015-04-04 04:23:30 +0000

    Put mines on field

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
+import random
+
+
 def main():
-    pass
+    field = [[None] * 10 for _ in range(10)]
+    for i in range(4):
+        field[random.randrange(10)][random.randrange(10)] = -1
+    for i in range(10):
+        for j in range(10):
+            if field[i][j] is None:
+                print(' ', end='')
+            elif field[i][j] == -1:
+                print('*', end='')
+            else:
+                print(field[i][j], end='')
+        print()
 
 if __name__ == '__main__':
     main()
