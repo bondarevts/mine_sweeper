3542729ce7472351f70a3293f8e32bc4f8fb2b7d
Timofey Bondarev <bondarevts@gmail.com>
2015-04-04 05:46:18 +0000

    Open all null positions in one step

--- main.py
+++ main.py
@@ -55,6 +55,32 @@ def main():
         if field[i][j] < 0:
             continue
 
+        if field[i][j] == 0:
+            check_pos = [(i, j)]
+            while check_pos:
+                last = check_pos.pop()
+                ci = last[0]
+                cj = last[1]
+                if 0 <= ci < 10 and 0 <= cj < 10:
+                    if field[ci][cj] == 0:
+                        check_pos.extend((ii, jj) for ii in range(ci-1, ci + 2) for jj in range(cj-1, cj+2) if not (ci == ii and cj == jj))
+                    if field[ci][cj] >= 0:
+                        field[ci][cj] = -field[ci][cj] - 1
+                        closed -= 1
+            print('-'*12)
+            for i in range(10):
+                print('|', end='')
+                for j in range(10):
+                    if field[i][j] >= 0:
+                        print(' ', end='')
+                    elif field[i][j] == -1:
+                        print('_', end='')
+                    else:
+                        print(-field[i][j] - 1, end='')
+                print('|')
+            print('-'*12)
+            continue
+
         field[i][j] = -field[i][j] - 1
         closed -= 1
 
