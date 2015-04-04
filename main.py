9350ded65f7fd94553d91556bdf1c1f810cb23cd
Timofey Bondarev <bondarevts@gmail.com>
2015-04-04 04:36:51 +0000

    Calc mines count in neighborhood positions

--- main.py
+++ main.py
@@ -2,18 +2,38 @@ import random
 
 
 def main():
-    field = [[None] * 10 for _ in range(10)]
+    field = [[0] * 10 for _ in range(10)]
     for i in range(4):
         field[random.randrange(10)][random.randrange(10)] = -1
     for i in range(10):
         for j in range(10):
-            if field[i][j] is None:
+            if field[i][j] == 0:
                 print(' ', end='')
             elif field[i][j] == -1:
                 print('*', end='')
             else:
                 print(field[i][j], end='')
         print()
+
+    for i in range(10):
+        for j in range(10):
+            if field[i][j] == -1:
+                for ii in range(i - 1, i + 2):
+                    for jj in range(j - 1, j + 2):
+                        if 0 <= ii < 10 and 0 <= jj < 10 and field[ii][jj] != -1:
+                            field[ii][jj] += 1
+
+    print('-' * 10)
+    for i in range(10):
+        for j in range(10):
+            if field[i][j] == 0:
+                print(' ', end='')
+            elif field[i][j] == -1:
+                print('*', end='')
+            else:
+                print(field[i][j], end='')
+        print()
+
 
 if __name__ == '__main__':
     main()
